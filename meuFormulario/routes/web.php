9950966a69ff18d95643e0f9ba7f89979908dc88
luizanmeneses <luizanmeneses@gmail.com>
2024-10-30 11:02:58 +0000

    space

--- meuFormulario/routes/web.php
+++ meuFormulario/routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::resource('users', FormControllers::class);
 
 
+
+
+
+
 /*
 Exemplos com função ao invés do Controller:
 1. Route::get('/users/delete', function () {
